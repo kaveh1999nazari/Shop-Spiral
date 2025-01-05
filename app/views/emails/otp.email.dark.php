d3345f915e322c99723404183f08b2b4420ceb8a
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-05 17:34:32 +0330

    add otp mailer view

---
+++ app/views/emails/otp.email.dark.php
@@ -0,0 +1,11 @@
+
+<extends:sendit:builder subject="Your Login Code"/>
+<use:bundle path="sendit:bundle"/>
+
+<block:html>
+    <p>Hi {{ $name }}!</p>
+    <p>Your code is: <strong>{{ $otp }}</strong></p>
+    <p>This code is valid for 3 minutes.</p>
+    <p>Best regards,<br>Spiral gRPC Team</p>
+</block:html>
+
