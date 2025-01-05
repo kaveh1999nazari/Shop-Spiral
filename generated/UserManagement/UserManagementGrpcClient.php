3c57dad9c0c3a2e47296bddb41dfec58e9f2c20b
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-05 16:17:14 +0330

    update UserManagementGrpcClient and extends GrpcClient

--- generated/UserManagement/UserManagementGrpcClient.php
+++ generated/UserManagement/UserManagementGrpcClient.php
@@ -4,35 +4,19 @@ declare(strict_types=1);
 
 namespace GRPC\UserManagement;
 
-use Spiral\Core\InterceptableCore;
-use Spiral\RoadRunner\GRPC\ContextInterface;
-
-class UserManagementGrpcClient implements UserManagementGrpcInterface
+/**
+ * @method Create(CreateUserRequest $request, string $responseClass, array $context = [])
+ * @method Update(UpdateUserRequest $request, string $responseClass, array $context = [])
+ */
+class UserManagementGrpcClient extends GrpcClient
 {
-    public function __construct(
-        private readonly InterceptableCore $core,
-    ) {
+    public function getServiceInterface(): string
+    {
+        return UserManagementGrpcInterface::class;
     }
 
-    public function Create(ContextInterface $ctx, CreateUserRequest $in): CreateUserResponse
+    public function getServiceName(): string
     {
-        [$response, $status] = $this->core->callAction(UserManagementGrpcInterface::class, '/'.self::NAME.'/Create', [
-            'in' => $in,
-            'ctx' => $ctx,
-            'responseClass' => \GRPC\UserManagement\CreateUserResponse::class,
-        ]);
-
-        return $response;
-    }
-
-    public function Update(ContextInterface $ctx, UpdateUserRequest $in): UpdateUserResponse
-    {
-        [$response, $status] = $this->core->callAction(UserManagementGrpcInterface::class, '/'.self::NAME.'/Update', [
-            'in' => $in,
-            'ctx' => $ctx,
-            'responseClass' => \GRPC\UserManagement\UpdateUserResponse::class,
-        ]);
-
-        return $response;
+        return UserManagementGrpcInterface::NAME;
     }
 }
