427b6ab3961fc4ddcfda8be733a3516e166f46cc
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-06 10:26:08 +0330

    .

--- runtime/cache/i18n.en.php
+++
@@ -1,6 +0,0 @@
-<?php return array (
-  'messages' => 
-  array (
-    'The PHP Framework for future Innovators' => 'The PHP Framework for future Innovators',
-  ),
-);
--- runtime/cache/i18n.locales.php
+++ runtime/cache/i18n.locales.php
@@ -1,4 +1,2 @@
 <?php return array (
-  0 => 'ru',
-  1 => 'en',
 );
