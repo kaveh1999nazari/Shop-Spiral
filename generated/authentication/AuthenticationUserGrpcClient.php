b0e27daaef5a937d45df6f76d5a1232004c3a2a4
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-11 09:11:20 +0330

    add method for GRPCClient

--- generated/authentication/AuthenticationUserGrpcClient.php
+++ generated/authentication/AuthenticationUserGrpcClient.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 namespace GRPC\authentication;
 
+use GRPC\UserManagement\CreateUserResidentRequest;
 use Spiral\Core\InterceptableCore;
 use Spiral\RoadRunner\GRPC\ContextInterface;
 
+/**
+ * @method LoginByMobile(LoginMobileRequest $request, $responseClass, $context = [])
+ * @method LoginByEmail(LoginEmailRequest $request, $responseClass, $context = [])
+ * @method Logout(LogoutRequest $request, $responseClass, $context = [])
+ */
 class AuthenticationUserGrpcClient extends AuthenticationGrpcClient
 {
     public function getServiceInterface(): string
