d410dd4c3766a7e8c89932fcef762cb1eeeedb08
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-11 14:56:09 +0330

    add more methods to show when call it

--- generated/UserManagement/UserManagementGrpcClient.php
+++ generated/UserManagement/UserManagementGrpcClient.php
@@ -10,6 +10,8 @@ use GRPC\UserManagement\UserManagementGrpcInterface;
 /**
  * @method Create(CreateUserRequest $request, string $responseClass, array $context = [])
  * @method CreateResident(CreateUserResidentRequest $request, $responseClass, $context = [])
+ * @method CreateEducation(CreateUserEducationRequest $request, $responseClass, $context = [])
+ * @method CreateJob(CreateUserJobRequest $request, $responseClass, $context = [])
  * @method Update(UpdateUserRequest $request, string $responseClass, array $context = [])
  */
 class UserManagementGrpcClient extends UserGrpcClient
