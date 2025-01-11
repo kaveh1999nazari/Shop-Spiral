8cddfb2e4b1f27fd567694ca30f46f2d4a52b824
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-11 14:56:32 +0330

    add @param for UserResponseDTO

--- app/src/Domain/DataTransferObject/UserResponseDTO.php
+++ app/src/Domain/DataTransferObject/UserResponseDTO.php
@@ -4,8 +4,10 @@ namespace App\Domain\DataTransferObject;
 
 class UserResponseDTO extends BaseDTO
 {
-
-
+    /**
+     * @param int $id
+     * @param string $message
+     */
     public function __construct(public int $id, public string $message)
     {
         $this->id = $id;
